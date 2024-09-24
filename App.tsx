b21c8a8b8da9c6853a22c0639537fa6731bbf697
Caio Pratali <caio@pratali.com.br>
2024-09-24 16:38:31 -0300

    creating SignUp Screen

--- App.tsx
+++ App.tsx
@@ -11,13 +11,13 @@ import { ThemeProvider } from '@shopify/restyle';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { theme } from './src/theme/theme';
-import { LoginScreen } from './src/screens/auth/LoginScreen/LoginScreen';
+import { SignUpScreen } from './src/screens/auth/SignUpScreen/SignUpScreen';
 
 function App(): React.JSX.Element {
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
-        <LoginScreen />
+        <SignUpScreen />
       </ThemeProvider>
     </SafeAreaProvider>
   );
--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import { Box } from '../Box/Box';
 import { UseAppSafeArea } from '../../hooks/useAppSafeArea';
+import { Icon } from '../Icon/Icon';
+import { Text } from '../Text/Text';
 
 interface ScreenProps {
     children: React.ReactNode;
+    canGoBack?: boolean;
 }
 
-export function Screen({ children }: ScreenProps) {
+export function Screen({ children, canGoBack = false }: ScreenProps) {
 
     const { top } = UseAppSafeArea();
 
     return (
         <Box paddingHorizontal="s24" style={{ paddingTop: top }}>
+            {
+                canGoBack && (
+                    <Box mb="s24" flexDirection="row" alignItems="center">
+                        <Icon name="arrowLeft" color="primary" />
+                        <Text preset="paragraphMedium" semiBold ml="s8">
+                            Voltar
+                        </Text>
+                    </Box>
+                )
+            }
             {children}
         </Box>
     );
